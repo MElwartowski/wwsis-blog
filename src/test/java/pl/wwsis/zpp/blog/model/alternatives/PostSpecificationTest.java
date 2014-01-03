d23f3b46c489294aeb7ddd6148317e57e59a095a
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2014-01-03 16:17:33 +0100

    dodatkowe importy

--- src/test/java/pl/wwsis/zpp/blog/model/alternatives/PostSpecificationTest.java
+++ src/test/java/pl/wwsis/zpp/blog/model/alternatives/PostSpecificationTest.java
@@ -2,6 +2,10 @@ package pl.wwsis.zpp.blog.model.alternatives;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.Collection;
 
 
 public class PostSpecificationTest {
